ed6568dd34137fd3056936aab7f0d19825a8d206
nimit-pasricha <pasricha.nimit@gmail.com>
2025-09-18 08:38:24 -0500

    Delete element at index from DynamicArray.

--- solution/dynamic_array.c
+++ solution/dynamic_array.c
@@ -1,12 +1,14 @@
 #include "dynamic_array.h"
 
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * TODO: Implement all the methods with declarations in dynamic_array.h here,
  * before proceeding with your project
  */
 
+// Create a new DynamicArray with given initial capacity
 DynamicArray *da_create(size_t init_capacity) {
   DynamicArray *da = malloc(sizeof(DynamicArray));
   da->data = malloc(sizeof(char *) * init_capacity);
@@ -16,6 +18,7 @@ DynamicArray *da_create(size_t init_capacity) {
   return da;
 }
 
+// Add element to Dynamic Array at the end. Handles resizing if necessary
 void da_put(DynamicArray *da, const char *val) {
   if (da->size == da->capacity) {
     da->capacity *= 2;
@@ -25,10 +28,25 @@ void da_put(DynamicArray *da, const char *val) {
   da->data[da->size++] = val;
 }
 
+// Get element at an index (NULL if not found)
 char *da_get(DynamicArray *da, const size_t ind) {
-    if (ind < 0 || ind >= da->size) {
-        return NULL;
-    }
+  if (ind < 0 || ind >= da->size) {
+    return NULL;
+  }
 
-    return da->data[ind];
+  return da->data[ind];
+}
+
+// Delete Element at an index (handles packing)
+void da_delete(DynamicArray *da, const size_t ind) {
+  if (ind < 0 || ind >= da->size) {
+    return;
+  }
+
+  if (ind < da->size - 1) {
+    memmove(&da->data[ind], &da->data[ind + 1],
+            (da->size - ind - 1) * sizeof(char *));
+  }
+
+  da->size--;
 }
