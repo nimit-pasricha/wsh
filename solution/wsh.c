653c888176ac1c7ef4336cee09b3d8bd69685bbd
nimit-pasricha <pasricha.nimit@gmail.com>
2025-09-18 09:56:02 -0500

    Read input from stdin in interactive mode.
    Just prints it back to stdout for now.

--- solution/wsh.c
+++ solution/wsh.c
@@ -1,13 +1,14 @@
 #include "wsh.h"
-#include "dynamic_array.h"
-#include "utils.h"
-#include "hash_map.h"
 
-#include <stdio.h>
 #include <errno.h>
 #include <stdarg.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+#include "dynamic_array.h"
+#include "hash_map.h"
+#include "utils.h"
 
 int rc;
 HashMap *alias_hm = NULL;
@@ -18,10 +19,12 @@ HashMap *alias_hm = NULL;
 /**
  * @Brief Free any allocated global resources
  */
-void wsh_free(void)
-{
+void wsh_free(void) {
   // Free any allocated resources here
-  if (alias_hm != NULL) { hm_free(alias_hm); alias_hm = NULL; }
+  if (alias_hm != NULL) {
+    hm_free(alias_hm);
+    alias_hm = NULL;
+  }
 }
 
 /**
@@ -29,8 +32,7 @@ void wsh_free(void)
  *
  * @param return_code The exit code to return
  */
-void clean_exit(int return_code)
-{
+void clean_exit(int return_code) {
   wsh_free();
   exit(return_code);
 }
@@ -41,8 +43,7 @@ void clean_exit(int return_code)
  * @param msg The warning message format string
  * @param ... Additional arguments for the format string
  */
-void wsh_warn(const char *msg, ...)
-{
+void wsh_warn(const char *msg, ...) {
   va_list args;
   va_start(args, msg);
 
@@ -58,17 +59,14 @@ void wsh_warn(const char *msg, ...)
  * @param argv Array of argument strings
  * @return
  */
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
   alias_hm = hm_create();
   setenv("PATH", "/bin", 1);
-  if (argc > 2)
-  {
+  if (argc > 2) {
     wsh_warn(INVALID_WSH_USE);
     return EXIT_FAILURE;
   }
-  switch (argc)
-  {
+  switch (argc) {
     case 1:
       interactive_main();
       break;
@@ -90,8 +88,16 @@ int main(int argc, char **argv)
  * @Brief Interactive mode: print prompt and wait for user input
  * execute the given input and repeat
  */
-void interactive_main(void)
-{
+void interactive_main(void) {
+  int should_exit = 0;
+  while (!should_exit) {
+    printf("wsh> ");
+    char input[MAX_LINE];
+    if (fgets(input, MAX_LINE, stdin) == NULL) {
+      fprintf(stderr, "fgets error\n");
+    }
+    printf("%s", input);
+  }
   // TODO: Implement interactive mode here
 }
 
@@ -103,10 +109,9 @@ void interactive_main(void)
  * @return EXIT_SUCCESS(0) on success, EXIT_FAILURE(1) on error
  */
 
-int batch_main(const char *script_file)
-{
+int batch_main(const char *script_file) {
   // TODO: Implement batch mode here
-  printf("%s\n",script_file);
+  printf("%s\n", script_file);
   return 0;
 }
 
@@ -123,17 +128,14 @@ int batch_main(const char *script_file)
  * @param argv Array to store the parsed arguments (must be preallocated)
  * @param argc Pointer to store the number of parsed arguments
  */
-void parseline_no_subst(const char *cmdline, char **argv, int *argc)
-{
-  if (!cmdline)
-  {
+void parseline_no_subst(const char *cmdline, char **argv, int *argc) {
+  if (!cmdline) {
     *argc = 0;
     argv[0] = NULL;
     return;
   }
   char *buf = strdup(cmdline);
-  if (!buf)
-  {
+  if (!buf) {
     perror("strdup");
     clean_exit(EXIT_FAILURE);
   }
@@ -141,11 +143,9 @@ void parseline_no_subst(const char *cmdline, char **argv, int *argc)
   const size_t len = strlen(buf);
   if (len > 0 && buf[len - 1] == '\n')
     buf[len - 1] = ' ';
-  else
-  {
+  else {
     char *new_buf = realloc(buf, len + 2);
-    if (!new_buf)
-    {
+    if (!new_buf) {
       perror("realloc");
       free(buf);
       clean_exit(EXIT_FAILURE);
@@ -156,20 +156,17 @@ void parseline_no_subst(const char *cmdline, char **argv, int *argc)
 
   int count = 0;
   char *p = buf;
-  while (*p && *p == ' ')
-    p++; /* skip leading spaces */
+  while (*p && *p == ' ') p++; /* skip leading spaces */
 
-  while (*p)
-  {
+  while (*p) {
     char *token_start = p;
     char *token = NULL;
-    if (*p == '\'')
-    {
+    if (*p == '\'') {
       token_start = ++p;
       token = strchr(p, '\'');
-      if (!token)
-      {
-        /* Handle missing closing quote - Print `Missing closing quote` to stderr */
+      if (!token) {
+        /* Handle missing closing quote - Print `Missing closing quote` to
+         * stderr */
         wsh_warn(MISSING_CLOSING_QUOTE);
         free(buf);
         for (int i = 0; i < count; i++) free(argv[i]);
@@ -179,12 +176,9 @@ void parseline_no_subst(const char *cmdline, char **argv, int *argc)
       }
       *token = '\0';
       p = token + 1;
-    }
-    else
-    {
+    } else {
       token = strchr(p, ' ');
-      if (!token)
-        break;
+      if (!token) break;
       *token = '\0';
       p = token + 1;
     }
@@ -196,8 +190,7 @@ void parseline_no_subst(const char *cmdline, char **argv, int *argc)
       clean_exit(EXIT_FAILURE);
     }
     count++;
-    while (*p && (*p == ' '))
-      p++;
+    while (*p && (*p == ' ')) p++;
   }
   argv[count] = NULL;
   *argc = count;
